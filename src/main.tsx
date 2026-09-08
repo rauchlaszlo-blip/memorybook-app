91f527be285901fa2e39ac7b1e0f7d2a06aa88ea
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-08 13:37:06 +0200

    Separate WebMCP demo from market routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,21 @@ import { MyBooksPage } from './MyBooksPage.tsx'
 import { OwnerBookPage } from './OwnerBookPage.tsx'
 import { PageInviteEditorPage } from './PageInviteEditorPage.tsx'
 import { PublicPage } from './PublicPage.tsx'
+import { JoinPage } from './JoinPage.tsx'
+import { OrganizerContributionsPage } from './OrganizerContributionsPage.tsx'
+import { BookViewerPage } from './BookViewerPage.tsx'
 
 const path = window.location.pathname
 const ownerBookMatch = path.match(/^\/my-books\/([^/]+)$/)
 const pageInviteMatch = path.match(/^\/p\/([^/]+)$/)
 const publicPageMatch = path.match(/^\/share\/([^/]+)$/)
+const joinMatch = path.match(/^\/join\/([^/]+)$/)
+const organizerMatch = path.match(/^\/organizer\/([^/]+)\/contributions$/)
+const bookViewMatch = path.match(/^\/book\/([^/]+)\/view$/)
 
 const root = path === '/login'
   ? <AuthPage />
-  : path === '/my-books'
+  : path === '/' || path === '/my-books'
     ? <MyBooksPage />
     : ownerBookMatch
       ? <OwnerBookPage bookId={decodeURIComponent(ownerBookMatch[1])} />
@@ -22,6 +28,14 @@ const root = path === '/login'
         ? <PageInviteEditorPage token={decodeURIComponent(pageInviteMatch[1])} />
         : publicPageMatch
           ? <PublicPage token={decodeURIComponent(publicPageMatch[1])} />
-          : <App />
+          : joinMatch
+            ? <JoinPage token={decodeURIComponent(joinMatch[1])} />
+            : organizerMatch
+              ? <OrganizerContributionsPage bookId={decodeURIComponent(organizerMatch[1])} />
+              : bookViewMatch
+                ? <BookViewerPage bookId={decodeURIComponent(bookViewMatch[1])} />
+                : path === '/demo'
+                  ? <App />
+                  : <MyBooksPage />
 
 createRoot(document.getElementById('root')!).render(root)
