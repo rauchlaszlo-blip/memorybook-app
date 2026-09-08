3bc280bc790cd726d121d5ce33b4bf8e53334481
rauchlaszlo-blip <rauchlaszlo@gmail.com>
2026-09-08 12:02:04 +0200

    Route owner invite manager and invited page editor

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import './index.css'
 import App from './App.tsx'
 import { AuthPage } from './AuthPage.tsx'
 import { MyBooksPage } from './MyBooksPage.tsx'
+import { OwnerBookPage } from './OwnerBookPage.tsx'
+import { PageInviteEditorPage } from './PageInviteEditorPage.tsx'
 
 const path = window.location.pathname
+const ownerBookMatch = path.match(/^\/my-books\/([^/]+)$/)
+const pageInviteMatch = path.match(/^\/p\/([^/]+)$/)
 
 const root = path === '/login'
   ? <AuthPage />
   : path === '/my-books'
     ? <MyBooksPage />
-    : <App />
+    : ownerBookMatch
+      ? <OwnerBookPage bookId={decodeURIComponent(ownerBookMatch[1])} />
+      : pageInviteMatch
+        ? <PageInviteEditorPage token={decodeURIComponent(pageInviteMatch[1])} />
+        : <App />
 
 createRoot(document.getElementById('root')!).render(root)
